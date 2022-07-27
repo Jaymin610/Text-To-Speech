54e8425a6ab8d2497b0f0b03576f4b43c1e3d1df
jaymin@gmail.com <jaymin@gmail.com>
2022-07-27 17:45:36 +0530

    Campaign and Composer

--- user/models.py
+++ user/models.py
@@ -3,11 +3,10 @@ from django.db import models
 # Create your models here.
 
 class User1(models.Model):
+    user_name = models.CharField(max_length=32, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
-    username = models.CharField(max_length=255, blank=True, null=True)
     password = models.CharField(max_length=10, blank=True, null=True)
     phone_no = models.CharField(max_length=10, blank=True, null=True)
-    otp = models.CharField(max_length=6, default=359)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=False)
     updated_at = models.DateTimeField(auto_now=True, blank=False)
@@ -17,6 +16,7 @@ class Campaign(models.Model):
     user_key = models.ForeignKey(User1, on_delete=models.CASCADE, null=True)
     CampaignName = models.CharField(max_length = 255,null=True,blank=True)
     CampaignStatus = models.CharField(max_length=255, null=True, blank=True)
+
 
 class Services(models.Model):
     campaign_key = models.ForeignKey(Campaign, on_delete=models.CASCADE, null=True)
--- user/views.py
+++ user/views.py
@@ -1,9 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, "home.html")
 
+def dashboard(request):
+    u_name = request.session['uname']
+    u_id = User1.objects.get(user_name=u_name)
+    data = Campaign.objects.filter(user_key_id=u_id)
+    print(data)
+    return render(request, "dashboard.html", {'data':data})
+
+@csrf_exempt
+def register(request):
+    if request.method == 'POST':
+        u_name = request.POST['u-name']
+        email = request.POST['email']
+        password = request.POST['password']
+        phone_no = request.POST['phoneno']
+        uid = User1.objects.create(user_name=u_name, email=email, password=password, phone_no=phone_no)
+        return redirect('/login/')
+
+
+@csrf_exempt
 def login(request):
-    return render(request, "login.html")
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        uid = User1.objects.get(email=email)
+        if uid.email == email and uid.password == password:
+            print('redirect', {'user': uid})
+            request.session['uname'] = uid.user_name
+            # request.session['id'] = uid.id
+            # request.session['email'] = uid.email
+            # request.session['phone_no'] = uid.phone_no
+            return redirect('/dashboard/')
+        else:
+            message = 'Email And Password Invalid'
+            return redirect('/login/')
+    else:
+        return render(request, 'login.html')
+
+
+def logout(request):
+    if 'email' in request.session:
+        del request.session['email']
+        return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
+
+@csrf_exempt
+def addCamp(request):
+    if request.method == 'POST':
+        name = request.POST['camp-name']
+        number = request.POST['phone-number']
+        uid = User1.objects.get(phone_no=number)
+        Campaign.objects.create(CampaignName=name, CampaignStatus="Active", user_key_id=uid.id)
+        return redirect("/dashboard/")
+    else:
+        return render(request, 'AddCamp.html')
+
+
+@csrf_exempt
+def addComposer(request):
+    id = request.GET.get('unique')
+    print(id)
+    return render(request, 'AddCompo.html')
+
+
