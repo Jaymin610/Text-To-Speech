ccf50d5d60774fa6a70211e0fd7624daf4870998
Jaymin Gajera <80027116+Jaymin610@users.noreply.github.com>
2022-08-22 12:08:01 +0530

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-import os
-import zipfile
-
-zf = zipfile.ZipFile("myzipfile.zip", "w")
-
-for dirname, subdirs, files in os.walk("./user/songdir/Bonrix_SS/Employee_1"):
-    for filename in files:
-        zf.write(os.path.join(dirname, filename))
-zf.close()
